9c9355eb1be1383c6be886d817d42c81cc5e7321
Marijan Brvar <marijan.brvar@gmail.com>
2021-05-21 15:09:10 +0200

    Add call to controller

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import './ui/styles/main.scss';
 import View from './ui/view';
+import Model from './ui/model';
+import Controller from './ui/controller';
 
-function render() {
-  const view = new View();
-  return view;
-}
-
-render();
+const app = new Controller(new Model(), new View());
