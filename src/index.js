2d2641dbd32ea50edf7ff509e5c99d355f9d739b
Marijan Brvar <marijan.brvar@gmail.com>
2021-05-24 10:13:23 +0200

    Clear template default items

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import Tooltip from './ui/tooltip';
+import Project from './ui/project';
 
 // create tooltip
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
 tooltip.init();
+
+const proj = new Project();
+proj.init();
