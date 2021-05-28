47b12f9afd0789441629f8efb781a82f3547d3e4
Marijan Brvar <marijan.brvar@gmail.com>
2021-05-28 20:10:40 +0200

    Add side list screen

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import Screen from './ui/screen';
 const app = new Screen();
 
 const add = function add() {
-  Screen.addToList('#list', 'li', 'test');
-  Screen.buildForm('#list', 'New List', 'list', 'Add');
-  Screen.buildForm('#jobs', 'New Job', 'job', 'Add');
+  Screen.buildHeader('Things need to be done!');
+  Screen.buildSideList('Job list', [{ id: 1, name: 'Job 1', active: true }, { id: 2, name: 'Job 2', active: false }]);
 };
 add();
 console.log(app);
--- src/ui/screen.js
+++ src/ui/screen.js
@@ -1,14 +1,13 @@
-import './styles/app.scss';
-
+/* eslint-disable class-methods-use-this */
 class Screen {
   constructor() {
     this.container = document.querySelector('.container');
-    this.container.classList.add('d-flex', 'p-6');
+    this.container.classList.add('container-md', 'clearfix');
     this.jobs = document.createElement('div');
     this.list = document.createElement('div');
     this.list.setAttribute('id', 'list');
-    this.list.classList.add('mx-md-6');
-    this.jobs.classList.add('flex-1', 'mx-md-6');
+    this.list.classList.add('col-sm-12', 'col-md-4', 'float-left', 'p-2');
+    this.jobs.classList.add('col-sm-12', 'col-md-8', 'float-left', 'p-2');
     this.jobs.setAttribute('id', 'jobs');
     this.container.append(this.list, this.jobs);
   }
@@ -17,7 +16,7 @@ class Screen {
     const targetElement = document.querySelector(selector);
     const element = document.createElement(tagName);
     element.innerText = text;
-    element.classList.add('list-item');
+    element.classList.add('menu-item');
     targetElement.appendChild(element);
     return element;
   }
@@ -42,8 +41,71 @@ class Screen {
     button.innerText = btnText;
     form.append(input, button);
     targetEl.appendChild(form);
+
     return form;
   }
+
+  static buildHeader(title) {
+    const target = document.querySelector('BODY');
+    const header = document.createElement('div');
+    const headerItem = document.createElement('div');
+    const headerLink = document.createElement('a');
+    headerLink.href = '/';
+    headerLink.classList.add('Header-link');
+    header.classList.add('Header', 'mb-5');
+    headerItem.classList.add('Header-item');
+    headerLink.innerText = title;
+    header.appendChild(headerItem);
+    headerItem.appendChild(headerLink);
+    target.prepend(header);
+
+    return header;
+  }
+
+  static buildSideList(title, jobs) {
+    const target = document.querySelector('#list');
+    const nav = document.createElement('nav');
+    const menuTitle = document.createElement('span');
+    const jobForm = document.createElement('form');
+    const jobInput = document.createElement('input');
+    const jobButton = document.createElement('button');
+    jobButton.classList.add('btn', 'ml-1', 'btn-outline');
+    jobButton.type = 'submit';
+    jobButton.innerText = 'Add';
+    jobInput.classList.add('form-control', 'input-md');
+    jobForm.append(jobInput, jobButton);
+    jobForm.setAttribute('id', 'job-form');
+    menuTitle.classList.add('menu-heading');
+    menuTitle.setAttribute('id', 'menu-heading');
+    menuTitle.innerText = title;
+    nav.appendChild(menuTitle);
+    nav.classList.add('menu');
+    nav.setAttribute('aria-label', 'Person settings');
+    jobs.forEach((item) => {
+      const link = document.createElement('a');
+      link.classList.add('menu-item');
+      if (item.active) {
+        link.setAttribute('aria-current', 'page');
+      }
+      link.innerText = item.name;
+      link.setAttribute('id', item.id);
+      nav.appendChild(link);
+    });
+    target.append(nav, jobForm);
+    return nav;
+  }
+
+  // <nav class="menu" aria-label="Person settings">
+  //       <span class="menu-heading" id="menu-heading">Menu heading</span>
+  //       <a class="menu-item" href="#url" aria-current="page">Account</a>
+  //       <a class="menu-item" href="#url">Profile</a>
+  //       <a class="menu-item" href="#url">Emails</a>
+  //       <a class="menu-item" href="#url">Notifications</a>
+  //     </nav>
+  //     <form>
+  //       <input class="form-control input-md" type="text" placeholder="Small input" aria-label="Small input" />
+  //       <button class="btn-link" type="button">Add</button>
+  //     </form>
 }
 
 export { Screen as default };
